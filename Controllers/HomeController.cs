60264b6978434c2162a5387f66cb81e094d5eafc
Abdelrahman <abdoshady550@gmail.com>
2025-07-19 19:07:59 +0300

    TRY REST

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Authentication.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HomeController : ControllerBase
+	{
+		private readonly AppDbContext _context;
+
+		public HomeController(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		// GET: api/Order
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Department>>> GetOrders()
+		{
+			return await _context.Departments
+				.ToListAsync();
+		}
+	}
+}
